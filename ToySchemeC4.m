a295d4e77ad426a4f319d76b36c7c9573b6d9c24
SVU12 <mousset.hugo.fr@gmail.com>
2024-03-25 15:00:26 +0100

    correct

--- ToySchemeC4.m
+++ ToySchemeC4.m
@@ -10,7 +10,7 @@ function c=ToySchemeC4(Nx,Ny,Nt,T,c0,Ux,Uy,D)
 
     %Resolution numérique avec Diffusion et convection, et vitesse Ux,Uy (vent)
     for n=1:Nt
-        c(2:Nx+1,2:Ny+1)=c(2:Nx+1,2:Ny+1) + dt*D*(c(3:Nx+2,2:Ny+1)+c(1:Nx,2:Ny+1)-2*c(2:Nx+1,2:Ny+1))/dx^2 + dt*D*(c(2:Nx+1,3:Ny+2)+c(2:Nx+1,1:Ny)-2*c(2:Nx+1,2:Ny+1))/dy^2- dt*Ux(:,:,t).*(c(3:Nx+2,2:Ny+1)-c(1:Nx,2:Ny+1))/(2*dx) - dt*Uy(:,:,t).*(c(2:Nx+1,3:Ny+2)-c(2:Nx+1,1:Ny))/(2*dy);
+        c(2:Nx+1,2:Ny+1)=c(2:Nx+1,2:Ny+1) + dt*D*(c(3:Nx+2,2:Ny+1)+c(1:Nx,2:Ny+1)-2*c(2:Nx+1,2:Ny+1))/dx^2 + dt*D*(c(2:Nx+1,3:Ny+2)+c(2:Nx+1,1:Ny)-2*c(2:Nx+1,2:Ny+1))/dy^2- dt*Ux(:,:,n).*(c(3:Nx+2,2:Ny+1)-c(1:Nx,2:Ny+1))/(2*dx) - dt*Uy(:,:,n).*(c(2:Nx+1,3:Ny+2)-c(2:Nx+1,1:Ny))/(2*dy);
     end
 end
 
