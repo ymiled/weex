8db49044a0d5b4e78502e1f3a1e5bbada3211511
SVU12 <mousset.hugo.fr@gmail.com>
2024-03-25 14:28:57 +0100

    vf

--- ToySchemeC4.m
+++ ToySchemeC4.m
@@ -1,12 +1,6 @@
 
 
-
-
-
-
-
-
-function c=ToySchemeXY(Nx,Ny,Nt,T,c0,Ux,Uy,D)
+function c=ToySchemeC4(Nx,Ny,Nt,T,c0,Ux,Uy,D)
     dx=1/(Nx+1);
     dy=1/(Ny+1);
     dt=T/Nt;
@@ -14,11 +8,11 @@ function c=ToySchemeXY(Nx,Ny,Nt,T,c0,Ux,Uy,D)
     c(2:Nx+1,2:Ny+1)=c0; %On initialise avec les conditions initiales.
     %D le coeff de diffusion.
 
-    %Reosultion numérique avec Diffusion et convection, et vitesse Ux,Uy (vent)
+    %Resolution numérique avec Diffusion et convection, et vitesse Ux,Uy (vent)
     for n=1:Nt
-        c(2:Nx+1,2:Ny+1)=c(2:Nx+1,2:Ny+1) + dt*D*(c(3:Nx+2,2:Ny+1)+c(1:Nx,2:Ny+1)-2*c(2:Nx+1,2:Ny+1))/dx^2
+        c(2:Nx+1,2:Ny+1)=c(2:Nx+1,2:Ny+1) + dt*D*(c(3:Nx+2,2:Ny+1)+c(1:Nx,2:Ny+1)-2*c(2:Nx+1,2:Ny+1))/dx^2 + dt*D*(c(2:Nx+1,3:Ny+2)+c(2:Nx+1,1:Ny)-2*c(2:Nx+1,2:Ny+1))/dy^2- dt*Ux(:,:,t).*(c(3:Nx+2,2:Ny+1)-c(1:Nx,2:Ny+1))/(2*dx) - dt*Uy(:,:,t).*(c(2:Nx+1,3:Ny+2)-c(2:Nx+1,1:Ny))/(2*dy);
     end
-    c;
+end
 
 
 
