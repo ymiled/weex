bd54d49651187a1063b9a2b33cfbad97bd08b24b
Youssef Miled <youssef.miled7@gmail.com>
2024-03-25 14:13:05 +0100

    added quiver

--- code.m
+++ code.m
@@ -6,3 +6,12 @@ image = imread('map.png');
 
 % Display the image
 imshow(image);
+
+Ux = a.Ux;
+Uy = a.Uy;
+longitude = a.longitude;
+latitude = a.lattitude;
+
+for t=1:248
+    quiver(Ux(:, :, t), Uy(:, :, t))
+end
