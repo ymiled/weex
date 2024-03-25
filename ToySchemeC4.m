11f0746d9de7789016b94788be7a2bf85a842261
SVU12 <mousset.hugo.fr@gmail.com>
2024-03-25 16:19:33 +0100

    jeu de test

--- ToySchemeC4.m
+++ ToySchemeC4.m
@@ -1,5 +1,3 @@
-
-
 function c=ToySchemeC4(Nx,Ny,Nt,T,c0,Ux,Uy,D)
     dx=1/(Nx+1);
     dy=1/(Ny+1);
@@ -13,8 +11,6 @@ function c=ToySchemeC4(Nx,Ny,Nt,T,c0,Ux,Uy,D)
         c(2:Nx+1,2:Ny+1)=c(2:Nx+1,2:Ny+1) + dt*D*(c(3:Nx+2,2:Ny+1)+c(1:Nx,2:Ny+1)-2*c(2:Nx+1,2:Ny+1))/dx^2 + dt*D*(c(2:Nx+1,3:Ny+2)+c(2:Nx+1,1:Ny)-2*c(2:Nx+1,2:Ny+1))/dy^2- dt*Ux(:,:,n).*(c(3:Nx+2,2:Ny+1)-c(1:Nx,2:Ny+1))/(2*dx) - dt*Uy(:,:,n).*(c(2:Nx+1,3:Ny+2)-c(2:Nx+1,1:Ny))/(2*dy);
     end
 end
-
-
 
 
 
