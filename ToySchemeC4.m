c409bfcc536034bc45b31a4494fa5d5fc57356f6
SVU12 <mousset.hugo.fr@gmail.com>
2024-03-25 14:19:23 +0100

    v1

--- ToySchemeC4.m
+++ ToySchemeC4.m
@@ -12,6 +12,14 @@ function c=ToySchemeXY(Nx,Ny,Nt,T,c0,Ux,Uy,D)
     dt=T/Nt;
     c=zeros(Nx+2,Ny+2);
     c(2:Nx+1,2:Ny+1)=c0; %On initialise avec les conditions initiales.
+    %D le coeff de diffusion.
+
+    %Reosultion numérique avec Diffusion et convection, et vitesse Ux,Uy (vent)
+    for n=1:Nt
+        c(2:Nx+1,2:Ny+1)=c(2:Nx+1,2:Ny+1) + dt*D*(c(3:Nx+2,2:Ny+1)+c(1:Nx,2:Ny+1)-2*c(2:Nx+1,2:Ny+1))/dx^2
+    end
+    c;
+
 
 
 
