d66c8c99873a1e28ec3de24a1120011c61473513
SVU12 <mousset.hugo.fr@gmail.com>
2024-03-25 16:42:21 +0100

    dzq

--- code.m
+++ code.m
@@ -5,26 +5,26 @@ a = load("vent_1_mois.mat");
 image = imread('map.png');
 
 % Display the image
-imshow(image);
+%imshow(image);
 
 Ux = a.Ux;
 Uy = a.Uy;
 longitude = a.longitude;
 latitude = a.latitude;
 
-for t=1:248
-    quiver(Ux(:, :, t), Uy(:, :, t))
-end
+%
+% for t=1:248
+ %   quiver(Ux(:, :, t), Uy(:, :, t))
+%end
 
-Nx = 10;
-Ny = 10;
-Nt = 100;
-T = 1;
+Nx = 20;
+Ny = 20;
+Nt = 500;
+T = 3;
 c0 = zeros(Nx, Ny) + 10*eye(Nx, Ny);
 Ux = ones(Nx, Ny,Nt); % Composante x de la vitesse du vent (peut être modifié selon votre cas)
 Uy = zeros(Nx, Ny,Nt); % Composante y de la vitesse du vent (peut être modifié selon votre cas)
-D = 0.1; % Coefficient de diffusion (peut être modifié selon votre cas)
+D = 0; % Coefficient de diffusion (peut être modifié selon votre cas)
 
 % Appel de la fonction
 c = ToySchemeC4(Nx, Ny, Nt, T, c0, Ux, Uy, D)
-heatmap(c);
