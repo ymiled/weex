f7db2f1f32be34e8b3c7ae656a4ae3b70f54d5d2
Youssef Miled <youssef.miled7@gmail.com>
2024-03-25 16:41:36 +0100

    code propre

--- ToySchemeC4.m
+++ ToySchemeC4.m
@@ -4,12 +4,17 @@ function c=ToySchemeC4(Nx,Ny,Nt,T,c0,Ux,Uy,D)
     dt=T/Nt;
     c=zeros(Nx+2,Ny+2);
     c(2:Nx+1,2:Ny+1)=c0; %On initialise avec les conditions initiales.
-    %D le coeff de diffusion.
+%D le coeff de diffusion.
 
     %Resolution numérique avec Diffusion et convection, et vitesse Ux,Uy (vent)
-    figure()
+figure()
     for n=1:Nt
-        c(2:Nx+1,2:Ny+1)=c(2:Nx+1,2:Ny+1) + dt*D*(c(3:Nx+2,2:Ny+1)+c(1:Nx,2:Ny+1)-2*c(2:Nx+1,2:Ny+1))/dx^2 + dt*D*(c(2:Nx+1,3:Ny+2)+c(2:Nx+1,1:Ny)-2*c(2:Nx+1,2:Ny+1))/dy^2- dt*Uy(:,:,n).*(c(3:Nx+2,2:Ny+1)-c(1:Nx,2:Ny+1))/(2*dx) - dt*Ux(:,:,n).*(c(2:Nx+1,3:Ny+2)-c(2:Nx+1,1:Ny))/(2*dy);
+        laplacien_x = (c(3:Nx+2,2:Ny+1) + c(1:Nx,2:Ny+1) - 2 * c(2:Nx+1,2:Ny+1)) / dx^2;
+        laplacien_y = (c(2:Nx+1,3:Ny+2) + c(2:Nx+1,1:Ny) - 2 * c(2:Nx+1,2:Ny+1)) / dy^2;
+        derivee_x = (c(3:Nx+2,2:Ny+1) - c(1:Nx,2:Ny+1)) / (2*dx);
+        derivee_y = (c(2:Nx+1,3:Ny+2)-c(2:Nx+1,1:Ny)) / (2*dy);
+
+        c(2:Nx+1,2:Ny+1) = c(2:Nx+1,2:Ny+1) + dt * D * laplacien_x + dt * D * laplacien_y - dt * Ux(:,:,n) .* derivee_x - dt * Uy(:,:,n) .* derivee_y;
         heatmap(c);
         hold on;
         pause(0.01);
