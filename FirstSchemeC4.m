aec88b55b6ef26661fd289c09bcb60a81f7b29cc
SVU12 <mousset.hugo.fr@gmail.com>
2024-03-26 14:50:27 +0100

    v0

--- FirstSchemeC4.m
+++ FirstSchemeC4.m
@@ -2,9 +2,15 @@ function c = FirstScheme(N,X,Y,Nt,T,c0,Ux,Uy,D)
     dx=X/N;
     dy=Y/N; %On garde une grille de taille carré avec des cases rectangles
     dt=T/Nt;
-    c=c0; %On initialise souvent à zeros(N+2,N+2)
+    c=c0; %On initialise souvent à zeros(N+2,N+2), on considère que le milieu est pas pollué.
+    %Conditions aux bords ? On souhaite que la pollution s'échappe par les bords du carré.
+    %On considère que la pollution est nulle en dehors du carré.
+    %Ainsi les condtions limites sont c=0 sur les bords du carré. 
 
     for i=1:Nt
-        laplacien = (c(3:Nx+2,2:Ny+1) + c(1:Nx,2:Ny+1) + c(2:Nx+1,3:Ny+2) + c(2:Nx+1,1:Ny) - 4 * c(2:Nx+1,2:Ny+1))/ (dx * dy);
-
+        laplacien = (c(3:N+2,2:N+1) + c(1:N,2:N+1) + c(2:N+1,3:N+2) + c(2:N+1,1:N) - 4 * c(2:N+1,2:N+1))/ (dx * dy);
+        derivee_x = (c(3:N+2,2:N+1) - c(1:N,2:N+1)) / (2*dx);
+        derivee_y = (c(2:N+1,3:N+2)-c(2:N+1,1:N)) / (2*dy);
+        c(2:N+1,2:N+1) = c(2:N+1,2:N+1) + dt * (D * laplacien - Ux * derivee_x - Uy * derivee_y);
+        
     end
