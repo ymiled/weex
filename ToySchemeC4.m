232385d15118002be8a4d1daca7ff3e8aa154a79
Youssef Miled <youssef.miled7@gmail.com>
2024-03-26 14:31:31 +0100

    v first certif

--- ToySchemeC4.m
+++ ToySchemeC4.m
@@ -3,21 +3,17 @@ function c=ToySchemeC4(Nx,Ny,Nt,T,c0,Ux,Uy,D)
     dy=1/(Ny+1);
     dt=T/Nt;
     c=zeros(Nx+2,Ny+2);
-    c(2:Nx+1,2:Ny+1)=c0; %On initialise avec les conditions initiales.
-%D le coeff de diffusion.
+    c=c0; %On initialise avec les conditions initiales.
 
     %Resolution numérique avec Diffusion et convection, et vitesse Ux,Uy (vent)
-figure()
+    figure()
     for n=1:Nt
         laplacien = (c(3:Nx+2,2:Ny+1) + c(1:Nx,2:Ny+1) + c(2:Nx+1,3:Ny+2) + c(2:Nx+1,1:Ny) - 4 * c(2:Nx+1,2:Ny+1))/ (dx * dy);
-        laplacien_x = (c(3:Nx+2,2:Ny+1) + c(1:Nx,2:Ny+1) - 2 * c(2:Nx+1,2:Ny+1)) / dx^2;
-        laplacien_y = (c(2:Nx+1,3:Ny+2) + c(2:Nx+1,1:Ny) - 2 * c(2:Nx+1,2:Ny+1)) / dy^2;
         derivee_x = (c(3:Nx+2,2:Ny+1) - c(1:Nx,2:Ny+1)) / (2*dx);
         derivee_y = (c(2:Nx+1,3:Ny+2)-c(2:Nx+1,1:Ny)) / (2*dy);
 
-        c(2:Nx+1,2:Ny+1) = c(2:Nx+1,2:Ny+1) + dt * D * laplacien - dt * Ux(:,:,n) .* derivee_x - dt * Uy(:,:,n) .* derivee_y;
-        heatmap(c);
-        pause(0.01);
+        c(2:Nx+1,2:Ny+1) = max(0, c(2:Nx+1,2:Ny+1) + dt * D * laplacien - dt * Ux(:,:,n) .* derivee_x - dt * Uy(:,:,n) .* derivee_y);
+
     end
 end
 
--- code.m
+++ code.m
@@ -1,5 +1,5 @@
 close all;
-a = load("vent_1_mois.mat");
+a = load("vent_1_mois.mat")
 
 
 % Load the image
@@ -22,14 +22,18 @@ Nx = 10;
 Ny = 10;
 Nt = 200;
 T = 2;
-c0 = zeros(Nx, Ny) + 10*eye(Nx, Ny);
-Ux = ones(Nx, Ny,Nt); % Composante x de la vitesse du vent (peut être modifié selon votre cas)
-Uy = zeros(Nx, Ny,Nt); % Composante y de la vitesse du vent (peut être modifié selon votre cas)
+c0 = zeros(Nx, Ny) + 1*eye(Nx, Ny);
+Ux = zeros(Nx, Ny,Nt); % Composante x de la vitesse du vent (peut être modifié selon votre cas)
+Uy = ones(Nx, Ny,Nt); % Composante y de la vitesse du vent (peut être modifié selon votre cas)
 D = 0.1; % Coefficient de diffusion (peut être modifié selon votre cas)
 
 
-
-
+%quiver(Ux(:, :, 1), Uy(:, :, 1))
+test = ones(Nx, Ny);
+for i = 1: Ny
+    test(i, Ny) = 0;
+end
+%heatmap(test)
 % Appel de la fonction
 c = ToySchemeC4(Nx, Ny, Nt, T, c0, Ux, Uy, D)
 
