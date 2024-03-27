91c50557f2d220b6954bf76fb566569a586707d2
SVU12 <mousset.hugo.fr@gmail.com>
2024-03-27 14:11:56 +0100

    ajout code map

---
+++ map.m
@@ -0,0 +1,11 @@
+picture = imread("map.png");
+imshow(picture)
+hold on
+
+w=width(picture);
+h=height(picture);
+z = rand(96,62);
+overlay = imagesc([w/190 w-w/124],[h/124 h-h/124], z); % the x/y values are the centers of the first/last box
+colormap turbo;
+colorbar
+set(overlay, 'AlphaData', .2);
