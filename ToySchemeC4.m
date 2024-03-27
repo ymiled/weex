5f90587ae32d256171fb659fec29a7e544b8369d
Youssef Miled <youssef.miled7@gmail.com>
2024-03-27 14:48:45 +0100

    b

--- ToySchemeC4.m
+++ ToySchemeC4.m
@@ -5,8 +5,21 @@ function c=ToySchemeC4(Nx,Ny,Nt,T,c0,Ux,Uy,D)
     c=zeros(Nx+2,Ny+2);
     c=c0; %On initialise avec les conditions initiales.
 
+    picture = imread("map.png");
+    imshow(picture)
+    hold on
+
+    w=width(picture);
+    h=height(picture);
+    z = rand(96,62);
+    overlay = imagesc([w/190 w-w/124],[h/124 h-h/124], z); % the x/y values are the centers of the first/last box
+    colormap turbo;
+    colorbar
+    clim([0 0.5])
+    set(overlay, 'AlphaData', .2);
+
     %Resolution numérique avec Diffusion et convection, et vitesse Ux,Uy (vent)
-    for n=1:Nt
+        for n=1:Nt
         laplacien = (c(3:Nx+2,2:Ny+1) + c(1:Nx,2:Ny+1) + c(2:Nx+1,3:Ny+2) + c(2:Nx+1,1:Ny) - 4 * c(2:Nx+1,2:Ny+1))/ (dx * dy);
         derivee_x = (c(3:Nx+2,2:Ny+1) - c(1:Nx,2:Ny+1)) / (2*dx);
         derivee_y = (c(2:Nx+1,3:Ny+2)-c(2:Nx+1,1:Ny)) / (2*dy);
