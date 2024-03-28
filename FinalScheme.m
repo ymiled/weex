d7e9db103020753c1f2f783b2121bbc74787b745
SVU12 <mousset.hugo.fr@gmail.com>
2024-03-28 08:13:54 +0100

    up

---
+++ FinalScheme.m
@@ -0,0 +1,21 @@
+function m_air = FinalScheme(Nx,Ny,X,Y,Nt,T,p,Ux,Uy,D,S)
+    dx=X/Nx; %Avec Nx = 95 et Ny = 62
+    dy=Y/Ny;
+    dt=T/Nt;
+    m_air=zeros(Nx+2,Ny+2,p);
+    %m_sol=zeros(Nx+2,Ny+2,p); %Les masses aux bords restent nulles
+    for j=1:p
+        figure(j)
+        for n=1:Nt
+                laplacien = (m_air(3:Nx+2,2:Ny+1,j) +m_air(1:Nx,2:Ny+1,j) +m_air(2:Nx+1,3:Ny+2,j) +m_air(2:Nx+1,1:Ny,j) - 4 *m_air(2:Nx+1,2:Ny+1,j)) / (dx * dy);
+                derivee_x = (m_air(3:Nx+2,2:Ny+1,j) -m_air(1:Nx,2:Ny+1,j)) / (2*dx);
+                derivee_y = (m_air(2:Nx+1,3:Ny+2,j) -m_air(2:Nx+1,1:Ny,j)) / (2*dy);
+
+
+                m_air(2:Nx+1,2:Ny+1,j)=max(0,m_air(2:Nx+1,2:Ny+1,j)+dt*(D*laplacien-Ux(:,:,n).*derivee_x - Uy(:,:,n).*derivee_y+S(:,:,j)));
+            
+                heatmap(m_air(:,:,j)');
+                pause(0.01);
+        end
+    end
+end
---
+++ test.m
@@ -0,0 +1,17 @@
+Nx=95;
+Ny=62;
+Nt=248;
+T=3;
+p=1;
+X=100;
+Y=100;
+a=load('vent_1_mois.mat');
+Ux=10*a.Ux;
+Uy=10*a.Uy;
+D=0.1;
+S=zeros(Nx,Ny,p);
+S((Nx+1)/2,Ny/2,1)=10;
+
+
+
+m=FinalScheme(Nx,Ny,X,Y,Nt,T,p,Ux,Uy,D,S)
