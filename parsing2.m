6ded582e68f5679b59da5a24172c520beff5d480
Daihecyy <paul.saubusse@gmail.com>
2024-03-28 08:58:05 +0100

    Modification parsing2 prise en compte surface TCDD

--- parsing2.m
+++ parsing2.m
@@ -1,9 +1,10 @@
 %% Parsing des données de map.xslx
 
-T = readtable("map_phase2_source.xlsx");
-lst_polluants = ["x2_3_7_8TCDD" "Benz_ne" "Chlore" "ChromeEtCompos_s_exprim_sEnTantQueCr_" "Compos_sOrganiquesVolatilsNonM_thaniques_COVNM_" "CuivreEtCompos_s_exprim_sEnTantQueCu_" "FluorEtCompos_sInorganiques_enTantQueHF_" "FluorEtSesCompos_s_F_" "HydrocarburesAromatiquesPolycycliques_HAP_" "MercureEtCompos_s_exprim_sEnTantQueHg_" "NickelEtCompos_s_exprim_sEnTantQueNi_" "Particules_PM10_" "ZincEtCompos_s_exprim_sEnTantQueZn_"];
+T = readtable("map_phase2_source.xlsx")
+tcdd = "x2_3_7_8TCDD" 
+lst_polluants = ["Benz_ne" "Chlore" "ChromeEtCompos_s_exprim_sEnTantQueCr_" "Compos_sOrganiquesVolatilsNonM_thaniques_COVNM_" "CuivreEtCompos_s_exprim_sEnTantQueCu_" "FluorEtCompos_sInorganiques_enTantQueHF_" "FluorEtSesCompos_s_F_" "HydrocarburesAromatiquesPolycycliques_HAP_" "MercureEtCompos_s_exprim_sEnTantQueHg_" "NickelEtCompos_s_exprim_sEnTantQueNi_" "Particules_PM10_" "ZincEtCompos_s_exprim_sEnTantQueZn_"];
 lst_herbicides = ["Oxamyl100G_kg_kg_hectare_" "Glyphosate360G_L_L_hectare_"];
-lst_nocifs = [lst_herbicides lst_polluants];
+lst_nocifs = [tcdd lst_herbicides lst_polluants];
 lst_infos = ["id_grid" lst_herbicides lst_polluants];
 nbr_x = 95;
 nbr_y = 62;
@@ -16,9 +17,8 @@ for i=1:length(T.id_grid)
     end
     x = floor((id-1)/nbr_y)+1;
     y = mod(id-1, nbr_y)+1;
-    sources(x, y, :) = [(T{i, lst_herbicides} * surface) T{i, lst_polluants}];
+    sources(x, y, :) = [(T{i, tcdd}*surface) (T{i, lst_herbicides} * surface * 10000) T{i, lst_polluants}];
 end
-
 
 T_full = readtable("map_phase2_correc_precipit_temp.xlsx");
 rain_matrix = zeros(nbr_x, nbr_y);
